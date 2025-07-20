311a50c4c7feb2a59088691d44ff113de282ed09
Nichokas <nicolas.rodalv@educa.jcyl.es>
2025-07-20 18:08:58 +0200

    Migrated fuzz traget

--- fuzz/fuzz_targets/fn/decrypt.rs
+++ fuzz/fuzz_targets/fn/decrypt.rs
@@ -1,30 +1,36 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use kychacha_crypto::{bytes_to_secret_key, decrypt, encrypt, generate_keypair};
+use kychacha_crypto::{bytes_to_secret_key, encrypt, generate_keypair, decrypt_from_reader};
+use bincode::de::read::SliceReader;
 
 fuzz_target!(|data: &[u8]| {
     // init -----------
     let keypair = generate_keypair().unwrap();
-    let ciphertext = encrypt(keypair.public_key,b"Hello World").unwrap();
+    let ciphertext = encrypt(keypair.public_key, b"Hello World").unwrap();
     // ----------------
 
     // all is fuzz
     if let Ok(secret_key) = bytes_to_secret_key(&data.to_vec()) {
-        if let Ok(public_key) = decrypt(data, &secret_key) {
-            let _ = public_key;
+        let reader = SliceReader::new(data);
+        if let Ok(plaintext) = decrypt_from_reader(reader, &secret_key) {
+            let _ = plaintext;
         }
     }
 
     // only data is fuzz
-    if let Ok(public_key) = decrypt(data, &keypair.private_key) {
-            let _ = public_key;
+    {
+        let reader = SliceReader::new(data);
+        if let Ok(plaintext) = decrypt_from_reader(reader, &keypair.private_key) {
+            let _ = plaintext;
+        }
     }
 
     // only key is fuzz
     if let Ok(secret_key) = bytes_to_secret_key(&data.to_vec()) {
-        if let Ok(public_key) = decrypt(&ciphertext, &secret_key) {
-            let _ = public_key;
+        let reader = SliceReader::new(&ciphertext);
+        if let Ok(plaintext) = decrypt_from_reader(reader, &secret_key) {
+            let _ = plaintext;
         }
     }
 });
